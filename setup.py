98ca730af3863cfebc2be29bb5cf4b98ce6c8136
NISHAKAR T <nishakarnishakar06@gmail.com>
2026-06-25 23:25:01 +0530

    INITIAL SETUP COMPLETED

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical_chatbot",
+    version="0.0.1",
+    author="Nishakar T",
+    author_email="nishakarnishakar06@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p resources
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch app.py
+touch resources/trials.ipynb
+
+echo "Project structure created successfully."
